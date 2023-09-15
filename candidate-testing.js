120e71092d4397f8347a10fb8b03b2de03b332f4
Doyle Brown <dalpiad@gmail.com>
2023-09-15 17:02:24 -0500

    Added the part 1 code of the project.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "What is the answer to life, the universe, and everything?";
+let correctAnswer = 42;
 let candidateAnswer;
 
 
@@ -18,19 +18,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("what is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question("Hello " + candidateName + "!" + question)
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if(candidateAnswer == correctAnswer) {
+  console.log("Congratulations! I also like Douglas Adams")
+} else {
+  console.log("Incorrect. See you in 7.5M years")
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
